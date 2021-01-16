e5f0ed6b6e74988d66f1c6f26b529c8e1afdcf19
Luke Mason <lukjmason@gmail.com>
2021-01-16 10:40:36 -0700

    Get site logo

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
+//IMAGES
+import './images/world.png';
 
-// An example of how you tell webpack to use a CSS (SCSS) file
+//CSS FILE
 import './css/base.scss';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
