ab533420e584a31c7522b81a5045a5120a853a66
Luke Mason <lukjmason@gmail.com>
2021-01-16 10:53:01 -0700

    Create a book flight button

--- src/index.js
+++ src/index.js
@@ -3,7 +3,3 @@ import './images/world.png';
 
 //CSS FILE
 import './css/base.scss';
-
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
-
-console.log('This is the JavaScript entry file - your code begins here.');
